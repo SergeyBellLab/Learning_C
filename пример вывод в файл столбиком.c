a8d7a7c3b9077e4ed712b687ae7f79b36ff85e6b
SergeyBellLab <77236321+SergeyBellLab@users.noreply.github.com>
2026-01-10 21:59:19 +0700

    первое помещение

---
+++ пример вывод в файл столбиком.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int size = 20;
+    int count = 0;
+    
+    for (int i = 0; i < size; i++) {
+        count = count + 1;
+        printf("%.4lf   ", 10.55555);
+        if (count == 4) {
+            printf("\n");
+            count = 0;
+        } 
+        //printf("Элемент %d: %d\n", i, numbers[i]);
+    }
+}
+
